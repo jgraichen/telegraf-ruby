5ad302ff402326c1aa134071ec5a4e6d6cf27a9b
Jan Graichen <jgraichen@altimos.de>
2017-08-16 14:17:27 +0200

    Releas 0.4.0

--- lib/telegraf/version.rb
+++ lib/telegraf/version.rb
@@ -3,7 +3,7 @@
 module Telegraf
   module VERSION
     MAJOR = 0
-    MINOR = 3
+    MINOR = 4
     PATCH = 0
     STAGE = nil
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
