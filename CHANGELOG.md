a3ecb6a597f79359aa2d15aa6cde9ba09cef04b8
Jan Graichen <jgraichen@altimos.de>
2018-07-27 13:12:12 +0200

    Release 0.5.0

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 0.5.0
+
+* Remove `influxdb` not unnecessarily restrict users needing a specific influxdb client.
+
--- lib/telegraf/version.rb
+++ lib/telegraf/version.rb
@@ -3,7 +3,7 @@
 module Telegraf
   module VERSION
     MAJOR = 0
-    MINOR = 4
+    MINOR = 5
     PATCH = 0
     STAGE = nil
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
